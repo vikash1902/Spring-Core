a88568b2ab1f8ed5e50b88fffdcf6f64d1bb33be
VIKASH KUMAR MISHRA <87639872+vikash1902@users.noreply.github.com>
2022-08-04 14:47:47 +0530

    Create Run.java

---
+++ Ambiduity_in_constructor_injection/Run.java
@@ -0,0 +1,16 @@
+package ci;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Run1 {
+
+	public static void main(String[] args) {
+		ApplicationContext contxt = new ClassPathXmlApplicationContext("ci/config.xml");
+		Person p = (Person) contxt.getBean("person");
+		System.out.println(p);
+        Addition add = (Addition) contxt.getBean("add");
+        add.doSum();
+	}
+
+}
