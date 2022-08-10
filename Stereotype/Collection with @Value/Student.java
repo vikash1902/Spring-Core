5d939fb166495783f8d02296eeedb41b8e16efae
VIKASH KUMAR MISHRA <87639872+vikash1902@users.noreply.github.com>
2022-08-10 15:57:56 +0530

    Create Student.java

---
+++ Stereotype/Collection with @Value/Student.java
@@ -0,0 +1,41 @@
+package stereotype;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+
+	@Value("Vikash Mishra")
+	private  String studentName;
+	@Value("01")
+	private int studentId;
+	@Value("#{temp}")
+	private List<String> address; 
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public int getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+	
+	
+	public List<String> getAddress() {
+		return address;
+	}
+	public void setAddress(List<String> address) {
+		this.address = address;
+	}
+	@Override
+	public String toString() {
+		return "Student [studentName=" + studentName + ", studentId=" + studentId + ", address=" + address + "]";
+	}	
+}
