6cd276cb73e9f8c2f4ce53bbc14fa07887578123
VIKASH KUMAR MISHRA <87639872+vikash1902@users.noreply.github.com>
2022-08-12 17:17:25 +0530

    Create Client.java

---
+++ Getting object of bean without using xml file/using @Component Annotation/Client.java
@@ -0,0 +1,18 @@
+package javaconfig;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Client {
+
+	private static ApplicationContext ctx;
+
+	public static void main(String[] args) {
+		ctx = new AnnotationConfigApplicationContext(Test.class);
+		Student temp = ctx.getBean("first",Student.class);
+		System.out.println(temp);
+		temp.study();
+	
+	}
+
+}
