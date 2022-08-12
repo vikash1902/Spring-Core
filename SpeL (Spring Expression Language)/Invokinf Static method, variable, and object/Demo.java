da48a18b223044ca9e4fa8666f635bdc566a83b9
VIKASH KUMAR MISHRA <87639872+vikash1902@users.noreply.github.com>
2022-08-12 12:37:06 +0530

    Create Demo.java

---
+++ SpeL (Spring Expression Language)/Invokinf Static method, variable, and object/Demo.java
@@ -0,0 +1,68 @@
+package spel;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Demo {
+	@Value("#{52+98}")
+	private int x;
+	@Value("#{35+62+65+35}")
+	private int y;
+	
+	// calling Static method
+	@Value("#{T(java.lang.Math).sqrt(4464)}")
+	private double z;
+	// calling static variable
+	@Value("#{T(java.lang.Math).E}")
+	private double v;
+	// Passing Object 
+	@Value("#{new java.lang.String('Vikash')}")
+	private String name;
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public double getZ() {
+		return z;
+	}
+
+	public void setZ(double z) {
+		this.z = z;
+	}
+
+	public double getV() {
+		return v;
+	}
+
+	public void setV(double v) {
+		this.v = v;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Demo [x=" + x + ", y=" + y + ", z=" + z + ", v=" + v + ", name=" + name + "]";
+	}
+
+}
