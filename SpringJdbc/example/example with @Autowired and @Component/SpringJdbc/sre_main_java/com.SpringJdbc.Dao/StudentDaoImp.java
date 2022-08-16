b4df82e261f12bee8df173ef0575edfc83ac093d
VIKASH KUMAR MISHRA <87639872+vikash1902@users.noreply.github.com>
2022-08-16 17:29:01 +0530

    Create StudentDaoImp.java

---
+++ SpringJdbc/example/example with @Autowired and @Component/SpringJdbc/sre_main_java/com.SpringJdbc.Dao/StudentDaoImp.java
@@ -0,0 +1,61 @@
+package com.SpringJdbc.dao;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+import com.SpringJdbc.entities.Student;
+@Component("studentDao")
+public class StudentDaoImp implements StudentDao {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	public int insert(Student student) {
+		// insert Query
+		String query = "insert into student(std_id,std_name,std_city) values(?,?,?)";
+		int r = this.jdbcTemplate.update(query, student.getStd_id(), student.getStd_name(), student.getStd_city());
+		return r;
+	}
+
+	public int change(Student student) {
+		// update Query
+		String query = "update student set std_name = ?, std_city = ? where std_id = ?";
+		int r = this.jdbcTemplate.update(query, student.getStd_name(), student.getStd_city(), student.getStd_id());
+		return r;
+	}
+
+	public int delete(Student student) {
+		// Delete Operation
+		String query = "delete from student where std_id = ?";
+		int r = this.jdbcTemplate.update(query, student.getStd_id());
+		return r;
+	}
+
+	public Student getStudent(int studentId) {
+		// Selecting Single Object
+		String query = "select * from student where std_id = ?";
+		RowMapper<Student> rowMapper = (RowMapper<Student>) new RowMapperImp();
+		Student r = this.jdbcTemplate.queryForObject(query, rowMapper, studentId);
+		return r;
+	}
+
+	public List<Student> getAllStudent() {
+		// Select all student
+		String query = "select * from student ";
+		List<Student> students = this.jdbcTemplate.query(query, new RowMapperImp());
+		return students;
+	}
+
+	public JdbcTemplate getJdbcTemplate() {
+		return jdbcTemplate;
+	}
+
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+}
