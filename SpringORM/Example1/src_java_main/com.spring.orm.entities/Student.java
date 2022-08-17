0f443976293a89c493f1657e3eab75a8cf78c13d
VIKASH KUMAR MISHRA <87639872+vikash1902@users.noreply.github.com>
2022-08-17 17:34:00 +0530

    Create Student.java

---
+++ SpringORM/Example1/src_java_main/com.spring.orm.entities/Student.java
@@ -0,0 +1,47 @@
+package com.spring.orm.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "student")
+public class Student {
+	@Id
+	@Column(name = "std_id")
+	private int studentId;
+	@Column(name = "std_name")
+	private String studentName;
+	@Column(name = "std_city")
+	private String studentCity;
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Student(int studentId, String studentName, String studentCity) {
+		super();
+		this.studentId = studentId;
+		this.studentName = studentName;
+		this.studentCity = studentCity;
+	}
+	public int getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public String getStudentCity() {
+		return studentCity;
+	}
+	public void setStudentCity(String studentCity) {
+		this.studentCity = studentCity;
+	}
+	
+}
